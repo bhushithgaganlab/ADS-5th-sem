e6e5fc5e5a9ff43458f97c36fe01c27a616e7565
Bhushith Gagan <42802614+bhushithgaganlab@users.noreply.github.com>
2020-12-30 10:42:01 +0530

    Open-ended questions

---
+++ Open Ended/question2.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+ 
+void findUniqueDigitNos(int l, int r) {
+ for (int i = l ; i <= r ; i++) { 
+      int num = i; 
+      bool visited[10] = {false}; 
+      while (num){                            //for each value check if a digit has appeared before or not
+          if (visited[num % 10]) break; 
+          visited[num % 10] = true; 
+          num = num / 10; 
+      } 
+      if (num == 0) {
+        cout << i << " "; 
+      }
+  }
+  cout<<"\n";
+}
+
+int main() {
+  int l ,r;
+  cout<<"Enter the left value and right value: "<<endl;
+  cin >> l >> r;
+  findUniqueDigitNos(l, r);
+
+  return 0;
+}
